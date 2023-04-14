631651d27b68843447ed2cc17a0df19038032e62
krishreddie <cheemarlaashwanthreddy@gmail.com>
2023-04-13 23:41:40 -0500

    changes has been done

--- controllers/nike.js
+++ controllers/nike.js
@@ -10,7 +10,7 @@ exports.nike_list = async function(req, res) {
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
-    }
+    }jhji
     };
 
     //List of all nike
@@ -30,9 +30,25 @@ exports.nike_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: nike detail: ' + req.params.id);
 };
 // Handle nike create on POST.
-exports.nike_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: nike create POST');
-};
+exports.nike_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new nike();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"nike_type":"goat", "cost":12, "size":"large"}
+    document.nike = req.body.nike;
+    document.size = req.body.size;
+    document.cost = req.body.cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
 // Handle nike delete form on DELETE.
 exports.nike_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: nike delete DELETE ' + req.params.id);
@@ -41,3 +57,36 @@ res.send('NOT IMPLEMENTED: nike delete DELETE ' + req.params.id);
 exports.nike_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: nike update PUT' + req.params.id);
 };
+// VIEWS
+// Handle a show all view
+exports.nike_view_all_Page = async function(req, res) {
+    try{
+    thenike = await nike.find();
+    res.render('nike', { title: 'nike Search Results', results: thenike });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+
+    // / Handle nike create on POST.
+    exports.nike_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new nike();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"nike_type":"goat", "cost":12, "size":"large"}
+    document.nike_type = req.body.nike_type;
+    document.cost = req.body.cost;
+    document.size = req.body.size;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
--- routes/nike.js
+++ routes/nike.js
@@ -7,3 +7,10 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+var express = require('express');
+const nike_controlers= require('../controllers/nike');
+var router = express.Router();
+/* GET nike */
+router.get('/', nike_controlers.nike_view_all_Page );
+module.exports = router;
