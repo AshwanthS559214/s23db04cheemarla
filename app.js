086c7982acfb2c85c62d21f63ef21241609dda74
krishreddie <cheemarlaashwanthreddy@gmail.com>
2023-04-27 21:01:59 -0500

    part 3 added

--- app.js
+++ app.js
@@ -32,6 +32,21 @@ var LocalStrategy = require('passport-local').Strategy;
 
 
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }))
+
+
 var app = express();
 
 // view engine setup
